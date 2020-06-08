a10f9066d018ed2909f40dbd5dfa29aee8883aba
Ryan <omgitsgod@gmail.com>
2020-06-08 12:57:00 -0400

    Add server

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { applyMiddleware, applyRoutes } from "./utils";
+import routes from "./services";
+import middleware from "./middleware";
+import errorHandlers from "./middleware/errorHandlers";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+process.on("uncaughtException", e => {
+    console.log(e);
+    process.exit(1);
+});
+process.on("unhandledRejection", e => {
+    console.log(e);
+    process.exit(1);
+});
+
+const server = express();
+const { PORT = 3000 } = process.env;
+
+applyMiddleware(middleware, server);
+applyRoutes(routes, server);
+applyMiddleware(errorHandlers, server);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
