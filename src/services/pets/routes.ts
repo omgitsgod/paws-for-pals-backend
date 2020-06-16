a7a93da32de2053fdcb8b6018fbbed72cb5320cf
Ryan <omgitsgod@gmail.com>
2020-06-15 23:26:33 -0400

    Add route for /dogs

--- src/services/pets/routes.ts
+++ src/services/pets/routes.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
+import { getDogsByLocation } from "./PetsController";
 
 export default [
-    {
-        path: '/',
-        method: 'get',
-        handler: async (req: Request, res: Response) => {
-            res.send('Pets!');
+  {
+    path: "/",
+    method: "get",
+    handler: async (req: Request, res: Response) => {
+        res.send("Pets!");
+    },
+  },
+  {
+    path: "/dogs",
+    method: "get",
+    handler: async ({ query }: Request, res: Response) => {
+        let result;
+        if (query.location) {
+            result = await getDogsByLocation(query.location.toString());
         }
-    }
+        res.status(200).send(result);
+    },
+  },
 ];
