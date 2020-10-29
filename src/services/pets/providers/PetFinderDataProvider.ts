d4fe736e0a32b053a5731a491b8cc2261ed76ff5
Ryan <omgitsgod@gmail.com>
2020-10-29 11:16:28 -0400

    Update functions to use petfinder package instead of fetch

--- src/services/pets/providers/PetFinderDataProvider.ts
+++ src/services/pets/providers/PetFinderDataProvider.ts
@@ -1,24 +1,33 @@
 import fetch from "node-fetch";
+import { Client } from '@petfinder/petfinder-js';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const apiKey:string = process.env.PETFINDER_KEY!;
+const secret:string = process.env.PETFINDER_SECRET!;
+
+const client = new Client({ apiKey, secret });
 type qeuries = {
     location: string;
 }
 
 export const getDogs = async (q: qeuries) => {
-
-    const location = q.location || ""
-    const key = process.env.PETFINDER_KEY;
-    const baseURL = "https://api.petfinder.com/v2";
-    const url = `${baseURL}/animals?type=dogs&key=${key}&location=${location}`
-    const response = await fetch(url);
-    return await response.json();
+  const location = q.location || '';
+  const response = await client.animal.search({
+    type: 'Dog',
+    location: location,
+    distance: '10'
+  });
+  return response.data;
 };
 
 export const getCats = async (q: qeuries) => {
-  const location = q.location || "";
-  const key = process.env.PETFINDER_KEY;
-  const baseURL = "https://api.petfinder.com/v2";
-  const url = `${baseURL}/animals?type=cats&key=${key}&location=${location}`;
-  const response = await fetch(url);
-  return await response.json();
+  const location = q.location || '';
+  const response = await client.animal.search({
+    type: 'Cat',
+    location: location,
+    distance: '10',
+  });
+  return response.data;
 };
