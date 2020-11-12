af91148946a90f77036f9267025cb1ad65aa2e3c
Ryan <omgitsgod@gmail.com>
2020-11-12 11:35:36 -0500

    Add function getCatsByLocation

--- src/services/pets/PetsController.ts
+++ src/services/pets/PetsController.ts
@@ -1,4 +1,4 @@
-import { getDogs } from "./providers/PetFinderDataProvider";
+import { getDogs, getCats } from "./providers/PetFinderDataProvider";
 
 export const getDogsByLocation = async (zip: string) => {
     if (zip.length < 5) {
@@ -8,4 +8,13 @@ export const getDogsByLocation = async (zip: string) => {
     }
 
     return getDogs({location : zip})
+};
+export const getCatsByLocation = async (zip: string) => {
+  if (zip.length < 5) {
+    return {
+      animals: [],
+    };
+  }
+
+  return getCats({ location: zip });
 };
