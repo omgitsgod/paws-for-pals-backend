bde5b3f04b84493e93698d26099bd4d13b504cbb
Ryan <omgitsgod@gmail.com>
2020-11-12 11:34:49 -0500

    Add route for /Cat

--- src/services/pets/routes.ts
+++ src/services/pets/routes.ts
@@ -3,21 +3,32 @@ import { getDogsByLocation } from "./PetsController";
 
 export default [
   {
-    path: "/",
-    method: "get",
+    path: '/',
+    method: 'get',
     handler: async (req: Request, res: Response) => {
-        res.send("Pets!");
+      res.send('Pets!');
     },
   },
   {
-    path: "/Dog",
-    method: "get",
+    path: '/Dog',
+    method: 'get',
     handler: async ({ query }: Request, res: Response) => {
-        let result;
-        if (query.location) {
-            result = await getDogsByLocation(query.location.toString());
-        }
-        res.status(200).send(result);
+      let result;
+      if (query.location) {
+        result = await getDogsByLocation(query.location.toString());
+      }
+      res.status(200).send(result);
+    },
+  },
+  {
+    path: '/Cat',
+    method: 'get',
+    handler: async ({ query }: Request, res: Response) => {
+      let result;
+      if (query.location) {
+        result = await getDogsByLocation(query.location.toString());
+      }
+      res.status(200).send(result);
     },
   },
 ];
