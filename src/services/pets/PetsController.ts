20db7a34b832b246e9298e237112e212ea75737b
Ryan <omgitsgod@gmail.com>
2020-12-04 11:51:18 -0500

    Add distance to arguements

--- src/services/pets/PetsController.ts
+++ src/services/pets/PetsController.ts
@@ -1,20 +1,20 @@
 import { getDogs, getCats } from "./providers/PetFinderDataProvider";
 
-export const getDogsByLocation = async (zip: string) => {
-    if (zip.length < 5) {
+export const getDogsByLocation = async (location: string, distance: string) => {
+    if (location.length < 5) {
         return {
             animals: []
         }
     }
 
-    return getDogs({location : zip})
+    return getDogs({ location, distance })
 };
-export const getCatsByLocation = async (zip: string) => {
-  if (zip.length < 5) {
+export const getCatsByLocation = async (location: string, distance: string) => {
+  if (location.length < 5) {
     return {
       animals: [],
     };
   }
 
-  return getCats({ location: zip });
+  return getCats({ location, distance });
 };
