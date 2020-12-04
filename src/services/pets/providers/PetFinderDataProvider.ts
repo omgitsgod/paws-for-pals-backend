1f79146da9c32bdfa00653458f0bc65853e26d39
Ryan <omgitsgod@gmail.com>
2020-12-04 11:50:33 -0500

    Add distance to queries and change limit to 100

--- src/services/pets/providers/PetFinderDataProvider.ts
+++ src/services/pets/providers/PetFinderDataProvider.ts
@@ -9,27 +9,28 @@ const secret:string = process.env.PETFINDER_SECRET!;
 
 const client = new Client({ apiKey, secret });
 type qeuries = {
-    location: string;
+    location: string,
+    distance: string,
 }
 
 export const getDogs = async (q: qeuries) => {
-  const location = q.location || '';
+  const { location, distance } = q;
   const response = await client.animal.search({
     type: 'Dog',
-    location: location,
-    distance: '10',
-    limit: '10',
+    location,
+    distance,
+    limit: '100',
   });
   return response.data;
 };
 
 export const getCats = async (q: qeuries) => {
-  const location = q.location || '';
+  const { location, distance } = q;
   const response = await client.animal.search({
     type: 'Cat',
-    location: location,
-    distance: '10',
-    limit: '10',
+    location,
+    distance,
+    limit: '100',
   });
   return response.data;
 };
