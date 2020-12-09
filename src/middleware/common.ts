389293d9aa51d878c8850c77cd02050fd7606814
Ryan <omgitsgod@gmail.com>
2020-12-09 12:21:56 -0500

    Add handleFavicon to serve favicon

--- src/middleware/common.ts
+++ src/middleware/common.ts
@@ -1,14 +1,20 @@
 import { Router, urlencoded, json } from "express";
 import cors from "cors";
 import compression from "compression";
+import favicon from "serve-favicon";
+import path from "path";
 
 export const handleCors = (router: Router) => router.use(cors({ credentials: true, origin: true }));
 
 export const handleBodyRequestParsing = (router: Router) => {
-    router.use(urlencoded());
-    router.use(json());
+  router.use(urlencoded());
+  router.use(json());
 };
 
 export const handleCompression = (router: Router) => {
-    router.use(compression());
+  router.use(compression());
+};
+
+export const handleFavicon = (router: Router) => {
+  router.use(favicon(path.join(process.cwd(), '/public', 'favicon.ico')));
 };
