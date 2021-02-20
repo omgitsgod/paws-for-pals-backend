b97c8443ef8e7d02066af51bbbd1fc553c46f709
Ryan <omgitsgod@gmail.com>
2021-02-20 15:51:06 -0500

    Add log of process.env

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log(process.env)
+
 export const apiKey: string =  process.env.PETFINDER_KEY!;
 export const secret: string =  process.env.PETFINDER_SECRET!;
 export const port: string = process.env.PORT || '5000';
@@ -9,5 +11,5 @@ export const clientID: string = process.env.CLIENTID!;
 export const clientSecret: string = process.env.CLIENTSECRET!;
 export const host: string = process.env.HOST!;
 export const client: string = process.env.CLIENT!;
-export const mongoDB: string = process.env.MONGODB_URI || 'mongodb://localhost/pawsforpals';
 export const redisUrl: any = process.env.REDIS_URL!;
+export const mongoDB: string = process.env.MONGODB_URI || 'mongodb://localhost/pawsforpals';
