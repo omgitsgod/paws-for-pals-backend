e946f349b36dbfeea71c56b8116f0397c3e0a71b
Ryan <omgitsgod@gmail.com>
2021-03-06 14:10:43 -0500

    Add sessionSecret

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,6 +8,7 @@ export const port: string = process.env.PORT || '5000';
 export const clientID: string = process.env.CLIENTID!;
 export const clientSecret: string = process.env.CLIENTSECRET!;
 export const host: string = process.env.HOST!;
+export const sessionSecret: string = process.env.SESSIONSECRET!;
 export const clientLink: string = process.env.CLIENTLINK!;
 export const redisUrl: any = process.env.REDIS_URL!;
 export const mongoDB: string = process.env.MONGODB_URI || 'mongodb://localhost/pawsforpals';
