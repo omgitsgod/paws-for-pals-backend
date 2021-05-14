de3dd471ba6bcb8c313aa0e3fe5f4f9570279f57
Ryan <omgitsgod@gmail.com>
2021-05-14 11:29:47 -0400

    Add shelterRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import petRoutes from "../components/pets/petRoutes";
 import userRoutes from '../components/users/userRoutes';
 import favoriteRoutes from '../components/favorites/favoriteRoutes';
+import shelterRoutes from "../components/shelters/shelterRoutes";
 
-export default [...petRoutes, ...userRoutes, ...favoriteRoutes];
+export default [...petRoutes, ...userRoutes, ...favoriteRoutes, ...shelterRoutes];
