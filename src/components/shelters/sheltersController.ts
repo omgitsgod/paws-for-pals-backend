5faeb644b04c8e0d244af7e9a198e63b28624a16
Ryan <omgitsgod@gmail.com>
2021-05-22 10:14:18 -0400

    Add getShelters

--- src/components/shelters/sheltersController.ts
+++ src/components/shelters/sheltersController.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express';
-import { getShelterById } from './sheltersService';
+import { getShelterById, getSheltersById } from './sheltersService';
 
 export const getShelter = async ({ query }: Request, res: Response) => {
   const id = query.id?.toString();
   const result = await getShelterById({ id });
+  res.status(200).send(result);
+};
+
+export const getShelters = async ({ query }: Request, res: Response) => {
+  const ids = typeof query.ids === 'string' && JSON.parse(query.ids);
+  const result = await getSheltersById({ ids });
   res.status(200).send(result);
 };
