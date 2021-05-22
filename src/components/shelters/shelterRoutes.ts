8f0becd9a94294049abe2fc60014e5ed37fe99f7
Ryan <omgitsgod@gmail.com>
2021-05-22 10:13:48 -0400

    Add /shelters

--- src/components/shelters/shelterRoutes.ts
+++ src/components/shelters/shelterRoutes.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from 'express';
-import { getShelter } from './sheltersController';
+import { getShelter, getShelters } from './sheltersController';
 
 export default [
   {
     path: '/shelter',
     method: 'get',
     handler: getShelter,
+  },
+  {
+    path: '/shelters',
+    method: 'get',
+    handler: getShelters,
   },
 ];
